4c70552a976fc6e925e620a9ca0dd4cebf7453a6
matiMef <siemaniema2115@interia.pl>
2026-03-17 09:48:31 +0100

    scipy dct added

--- Kompresja_Obrazow/main.py
+++ Kompresja_Obrazow/main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import skimage.io as io
 import numpy as np
 import skimage.color as color
+from scipy.fftpack import dct, idct
 
 def crop_image(image, crop_size):
   h, w = image.shape
@@ -54,7 +55,7 @@ def dct_compression(img, img_h, img_w):
   T[1:, :] /= np.sqrt(M / 2)
   # test = T @ T.T
   # print(np.round(test))
-
+ 
   B = (T @ img_block @ T.T)
   # print("Po transformacji",B)  
   
@@ -65,6 +66,7 @@ def dct_compression(img, img_h, img_w):
       mask[i, j] = 1
   
   B_masked = B * mask
+  print(B_masked)
 
   A_compressed = T.T @ B_masked @ T
   return A_compressed
@@ -78,28 +80,57 @@ for i in range(img_h):
     block = dct_compression(img, i, j)
     img_compressed[i, j] = block 
 
-print(img_compressed)
-print(img_compressed.shape)
+# print(img_compressed)
+# print(img_compressed.shape)
 
+def scipy_dct(image_input):
+ with np.printoptions(edgeitems=8, precision=2, linewidth=1000):
+  B = dct(dct(image_input.T, norm='ortho').T, norm='ortho')
+  print(B)
+
+  mask_size = 8//2
+  mask = np.zeros((8,8))
+  for i in range (0, mask_size):
+    for j in range(mask_size-i):
+      mask[i, j] = 1
+    
+  B_compressed = B * mask
+  print(B_compressed)
+
+  A_compressed = idct(idct(B_compressed, norm='ortho').T, norm='ortho').T
+  return A_compressed
+
+img_compressed2 = np.zeros_like(img)
+for i in range(img_h):
+   for j in range(img_w):
+    # print(img.shape)
+    block2 = scipy_dct(img[i,j])
+    img_compressed2[i, j] = block2
+
+# scipy_dct(img[0,0])
+# dct_compression(img, 0, 0)
 
 def to_image(image, block_size):
   h = image.shape[0]
   w = image.shape[1]
-  print(h, w)
+  # print(h, w)
   temp = image.transpose(0, 2, 1, 3)
   combined = np.reshape(temp, (h * block_size, w * block_size))
-  
   return combined
 
 new_image = to_image(img_compressed, 8)
+new_image2 = to_image(img_compressed2, 8)
 
 plt.figure(figsize=(12, 6))
-plt.subplot(1, 2, 1)
+plt.subplot(1, 3, 1)
 plt.title("Przed kompresją")
 plt.imshow(image, cmap='gray')
-plt.subplot(1, 2, 2)
+plt.subplot(1, 3, 2)
 plt.title("Po kompresji")
 plt.imshow(new_image, cmap='gray')
+plt.subplot(1, 3, 3)
+plt.title("Po kompresji 2")
+plt.imshow(new_image2, cmap='gray')
 plt.show()
 
 # print(img.shape)
